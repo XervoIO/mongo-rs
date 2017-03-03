517930cbeb9a25c2f9c45ec4c9c2e6a6ed91855a
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2017-03-03 12:47:56 -0500

    perf: remove promt for standup type

--- schemas/scraper.js
+++ schemas/scraper.js
@@ -2,32 +2,32 @@ const Prompt = require('prompt')
 
 module.exports = {
   properties: {
-    confirm: {
+    enable: {
       description: 'Enable mongo metrics?',
       message: 'Enter true or false',
       required: true,
       type: 'boolean'
     },
     apiHost: {
-      ask: () => Prompt.history('confirm').value === true,
+      ask: () => Prompt.history('enable').value === true,
       description: 'Enter API hostname to send mongo stats',
       required: true
     },
     apiPort: {
-      ask: () => Prompt.history('confirm').value === true,
+      ask: () => Prompt.history('enable').value === true,
       default: 443,
       description: 'Enter API port to send mongo stats',
       required: false
     },
     apiTLS: {
-      ask: () => Prompt.history('confirm').value === true,
+      ask: () => Prompt.history('enable').value === true,
       default: true,
       description: 'API TLS enabled?',
       required: false,
       type: 'boolean'
     },
     apiToken: {
-      ask: () => Prompt.history('confirm').value === true,
+      ask: () => Prompt.history('enable').value === true,
       description: 'Enter API admin token',
       required: true
     }

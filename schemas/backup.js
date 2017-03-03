72fc1e959068b202b85e049a42bed9b63001436d
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2017-03-03 12:47:56 -0500

    perf: remove promt for standup type

--- schemas/backup.js
+++ schemas/backup.js
@@ -2,24 +2,24 @@ const Prompt = require('prompt')
 
 module.exports = {
   properties: {
-    confirm: {
+    enable: {
       description: 'Enable mongo backups?',
       message: 'Enter true or false',
       type: 'boolean',
       required: true
     },
     s3AccessKey: {
-      ask: () => Prompt.history('confirm').value === true,
+      ask: () => Prompt.history('enable').value === true,
       description: 'Enter AWS S3 access key',
       required: true
     },
     s3SecretKey: {
-      ask: () => Prompt.history('confirm').value === true,
+      ask: () => Prompt.history('enable').value === true,
       description: 'Enter AWS S3 secret key',
       required: true
     },
     s3Bucket: {
-      ask: () => Prompt.history('confirm').value === true,
+      ask: () => Prompt.history('enable').value === true,
       description: 'Enter AWS S3 bucket (folder path included)',
       message: 'Example bucket (mongo.backups/example-folder)',
       required: true
--- schemas/mongo.js
+++ schemas/mongo.js
@@ -1,8 +1,4 @@
-const mongo = {
-  types: ['primary', 'secondary', 'arbiter', 'singleton'],
-  versions: ['3.0.1', '3.0.2', '3.0.3', '3.2.0', '3.4.0', '3.4.2']
-}
-
+const versions = ['3.0.1', '3.0.2', '3.0.3', '3.2.0', '3.4.0', '3.4.2']
 const ipMessage = 'This is used for replica set configuration'
 
 module.exports = {
@@ -18,15 +14,9 @@ module.exports = {
       type: 'string'
     },
     version: {
-      conform: (value) => mongo.versions.indexOf(value) !== -1,
+      conform: (value) => versions.indexOf(value) !== -1,
       description: 'Enter a mongo version',
-      message: `Supported mongo versions (${mongo.versions.join(', ')})`,
-      required: true
-    },
-    type: {
-      conform: (value) => mongo.types.indexOf(value) !== -1,
-      description: 'Enter mongo standup type',
-      message: `Supported standup types (${mongo.types.join(', ')})`,
+      message: `Supported mongo versions (${versions.join(', ')})`,
       required: true
     },
     username: {
